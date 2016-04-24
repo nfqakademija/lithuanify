7378625e85bc57bb80551cc1d0f77465542cc194
DeividasZilinskas <zilinskas.de@gmail.com>
2016-04-24 17:27:37 +0300

    Remove web/js and web/css from .gitignore

---
+++ web/js/datePicker.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Rokas on 24/04/16.
+ */
+$('#range').datepicker({
+    format: "yyyy-mm-dd",
+    todayBtn: "linked",
+    language: "lt",
+    autoclose: true,
+    todayHighlight: true,
+    startDate: "1990-01-01",
+    endDate: "today"
+});
