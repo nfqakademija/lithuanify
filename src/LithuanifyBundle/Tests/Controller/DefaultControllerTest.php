b1f4f65afe963b1483504f9733e057b085653265
DeividasZilinskas <zilinskas.de@gmail.com>
2016-04-24 17:45:49 +0300

    Add LithuanifyBundle DefaultController tests

--- src/LithuanifyBundle/Tests/Controller/DefaultControllerTest.php
+++ src/LithuanifyBundle/Tests/Controller/DefaultControllerTest.php
@@ -6,8 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testResponseCode()
     {
-        $this->assertEquals(0, 0);
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), $client->getResponse()->getContent());
+    }
+    public function testIfFormExist()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertContains('<form', $client->getResponse()->getContent());
     }
 }
