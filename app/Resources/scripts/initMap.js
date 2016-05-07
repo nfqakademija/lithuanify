212d07ca0d9eaf689d297293deddb0494bec95cc
DeividasZilinskas <zilinskas.de@gmail.com>
2016-05-07 17:37:23 +0300

    Click on marker now shows articles from that country

--- app/Resources/scripts/initMap.js
+++ app/Resources/scripts/initMap.js
@@ -25,8 +25,32 @@ function initMap() {
             position: new google.maps.LatLng(test[i][1], test[i][2]),
             map: map,
             animation: google.maps.Animation.DROP,
+            title: test[i][0],
             icon: 'https://raw.githubusercontent.com/Concept211/Google-Maps-Markers/master/images/marker_black'+
             test[i][3].length+'.png'
         });
+        attachSecretMessage(marker, test[i][0], test[i][3]);
     }
+}
+
+function attachSecretMessage(marker, countryName, articles) {
+    var message = '<b>' + countryName + '</b><br />Iš viso strapsnių: <b>' + articles.length + '</b>';
+    var infowindow = new google.maps.InfoWindow({
+        content: message
+    });
+
+    marker.addListener('click', function() {
+        var i, articleList;
+
+        infowindow.open(marker.get('map'), marker);
+
+        var div = document.getElementById('js-load-articles');
+        for (i = 0; i < articles.length; i++) {
+            articleList += '<article class="read-toggle"><h4>' + articles[i][0].substring(0, 87) + '</h4>' +
+                '<img class="littleSpaceRight" width="105px" src="http://g2.dcdn.lt/images/pix/arunas-70820028.jpg" ' +
+                'style="float: left;" /> <p>' + articles[i][1].substring(0, 150) + '</p> </article>';
+        }
+
+        div.innerHTML = articleList;
+    });
 }
