2865ce3ad8fa297af36bb89e2421bc70b0d58013
DeividasZilinskas <zilinskas.de@gmail.com>
2016-05-09 23:21:05 +0300

    Fix auto submit

--- src/LithuanifyBundle/Controller/DefaultController.php
+++ src/LithuanifyBundle/Controller/DefaultController.php
@@ -30,13 +30,14 @@ class DefaultController extends Controller
         $form = $this->createForm(DatePick::class, $datePick);
         $form1 = $this->createForm(EventPick::class, $eventPick);
 
-        $form->handleRequest($request);
-        $rawArticles = array();
         $articleManager = $this->get('article_manager');
-        if ($form->isValid()) {
-            $articles = $articleManager->searchArticles($datePick);
-            $rawArticles = $articleManager->buildArray($articles);
-        }
+
+
+        $datePick->setBeginDate(new \DateTime());
+        $datePick->setEndDate(new \DateTime());
+
+        $articles = $articleManager->searchArticles($datePick);
+        $rawArticles = $articleManager->buildArray($articles);
 
         $form1->handleRequest($request);
         if ($form1->isValid()) {
@@ -47,61 +48,19 @@ class DefaultController extends Controller
         } else {
             $form->get('beginDate')->setData(new \DateTime());
             $form->get('endDate')->setData(new \DateTime());
-
-            if (!$form->isSubmitted() AND !$form1->isSubmitted()) {
-                $datePick->setBeginDate(new \DateTime());
-                $datePick->setEndDate(new \DateTime());
-                $articles = $articleManager->searchArticles($datePick);
-                $rawArticles = $articleManager->buildArray($articles);
-            }
         }
+
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $articles = $articleManager->searchArticles($datePick);
+            $rawArticles = $articleManager->buildArray($articles);
+        }
+
 
         return $this->render('LithuanifyBundle:Default:index.html.twig', [
             'form' => $form->createView(),
             'form1' => $form1->createView(),
             'articles' => $rawArticles,
         ]);
-    }
-
-    /**
-     * @Route("/data")
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function dataAction()
-    {
-        $apiKey = '5afb00834532417d9e6a033abe6dfc09938db9122c38aa67153f65608831ec19d77821d17f57a1392bf7dd9715a4eb154b71c51886e00681b5924833b907523cff1f1edb5869b759ed2a5ec091fae469';
-        $outer = 'https://extraction.import.io/query/runtime/051c1d72-4841-4917-8fe7-7f2f92419229?_apikey=';
-        $inner = 'https://extraction.import.io/query/runtime/22f06d81-5536-477c-bbeb-7c848bc992ba?_apikey=';
-        $outerFirstSource = 'http://www.mk.ru/search/?q=%D0%BB%D0%B8%D1%82%D0%B2%D0%B0';
-
-        $outerUrl = $outer.$apiKey.'&url='.$outerFirstSource;
-        $firstPage = json_decode(file_get_contents($outerUrl));
-
-        foreach ($firstPage->extractorData->data[0]->group[0]->LINK as $source) {
-            $a = file_get_contents($inner.$apiKey.'&url='.$source->href);
-            $a = json_decode($a);
-
-            foreach ($a->extractorData->data[0]->group as $key => $value) {
-                if ($key == 0) {
-                    $title = $value->Title[0]->text;
-                    $content = "";
-                } else {
-                    $content .= $value->Data[0]->text . '<br>';
-                }
-            }
-
-            $article = new Article();
-            $article->setTitle($title);
-            $article->setContent($content);
-            $article->setNewsUrl($source->href);
-            $article->setEventId(1);
-            $article->setSourceId(1);
-            $article->setDate(123);
-            $article->setCountryId(1);
-
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($article);
-            $em->flush();
-        }
     }
 }
